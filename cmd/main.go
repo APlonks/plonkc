0397c3fee918d5f6afbe10520b47f67bffe54ec6
APlonks <antoine.planque@gmail.com>
2025-08-07 21:33:19 +0000

    Bash in go

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	cmd := exec.Command("/bin/bash")
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/APlonks/plonkc
+
+go 1.24.4
